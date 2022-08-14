adc34402f353147ee7194481fd6de930d5018f36
David <david26ly@gmail.com>
2022-08-14 00:09:14 -0400

    added styling and picture

--- src/App.js
+++ src/App.js
@@ -17,33 +17,40 @@ function App() {
 
   return (
     <div className="App">
-      <div className="search">
+      <div className="search-box">
         <input
           type="text"
           value={location}
           onChange={(e) => setLocation(e.target.value)}
           onKeyPress={findLocation}
           placeholder='Enter a location'
+          className="search-bar"
         />
       </div>
       <div className="container">
-        <div className="location">
-          {data.sys ? <p>{data.name}, {data.sys.country}</p> : null}
+        <div className="location-box">
+          <div className="location">
+            {data.sys ? <p>{data.name}, {data.sys.country}</p> : null}
+          </div>
+        </div>
+        <div className="weather-box">
           <div className="temp">
             {data.main ? <h1>{(data.main.temp - 273.15).toFixed(2)}°C</h1> : null}
           </div>
           <div className="description">
-            {data.weather ? <p>Description: {data.weather[0].description}</p> : null}
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
           </div>
         </div>
-        <div className="feels_like">
-          {data.main ? <p>Feels Like: {(data.main.feels_like - 273.15).toFixed(2)}°C</p> : null}
-        </div>
-        <div className="humidity">
-          {data.main ? <p>Humidity: {data.main.humidity}%</p> : null}
-        </div>
-        <div className="wind">
-          {data.wind ? <p>Wind speed: {(data.wind.speed * 3.6).toFixed(2)}km/h</p> : null}
+        <div className="bottom">
+          <div className="feels_like">
+            {data.main ? <p>Feels Like: {(data.main.feels_like - 273.15).toFixed(2)}°C</p> : null}
+          </div>
+          <div className="humidity">
+            {data.main ? <p>Humidity: {data.main.humidity}%</p> : null}
+          </div>
+          <div className="wind">
+            {data.wind ? <p>Wind speed: {(data.wind.speed * 3.6).toFixed(2)}km/h</p> : null}
+          </div>
         </div>
       </div>
     </div>
